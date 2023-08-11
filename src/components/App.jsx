c2d0742f8e1c8b9cb83419a632ed2f507088a167
Rhilim <arhilim@gmail.com>
2023-08-11 04:17:42 -0500

    task 1 logic added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { FeedbackOptions } from './Feedback/FeedbackOptions';
+import { Notification } from './Feedback/Notification';
 import { Section } from './Feedback/Section';
 import { Statistics } from './Feedback/Statistics';
 
@@ -32,23 +33,31 @@ export class App extends Component {
   };
 
   render() {
+    const { good, neutral, bad } = this.state;
+
+    const allParamsAreZero = good === 0 && neutral === 0 && bad === 0;
+
     return (
       <>
-      <Section title="Please leave feedback" children={this.children}>
-        <FeedbackOptions
-          options={this.state} 
-          onLeaveFeedback={this.handleClick}
-        />
+        <Section title="Please leave feedback" children={this.children}>
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleClick}
+          />
         </Section>
         <Section title="Statistics" children={this.children}>
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        />
-      </Section>
+          {allParamsAreZero ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
       </>
     );
   }
