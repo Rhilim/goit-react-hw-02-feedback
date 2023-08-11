1102d65f31d97339ae8abc4fc239a3bff256d16c
Rhilim <arhilim@gmail.com>
2023-08-11 15:49:29 -0500

    some styles added

--- src/components/Feedback/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions.styled.jsx
@@ -1,18 +1,17 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const FeedbackOptionsS = styled.button`
-margin:5px;
-padding: 8px;
-border: 1px solid grey;
-border-radius: 5px;
-font-size: 1em;
-color: #5F9EA0;
-background-color: #FFE4C4;
-box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-:hover{
+  margin: 5px;
+  padding: 8px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  font-size: 1em;
+  color: #5f9ea0;
+  background-color: #ffe4c4;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  :hover {
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    color: #00008B;
-background-color: #FFF8DC;
-}
-`
-
+    color: #00008b;
+    background-color: #fff8dc;
+  }
+`;
